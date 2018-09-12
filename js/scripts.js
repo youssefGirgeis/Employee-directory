5f9b0ccdb495e4e47805157d35e01e9fec7bbd53
Youssef Girgeis <youssefgirgeis@gmail.com>
2018-09-11 23:21:22 -0400

    Added the search container

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,14 @@
-$.ajax({
-    url: 'https://randomuser.me/api/',
-    dataType: 'json',
-    success: function(data){
-        console.log(data.results);
-    }
-});
+const $searchContainer =  $('.search-container');
+$searchContainer.append('<form action="#" method="get"></form>');
+$('form').append('<input type="search" id="search-input" class="search-input" placeholder="Search...">');
+$('form').append('<input type="submit" value="&#x1F50D;" id="serach-submit" class="search-submit">');
+
+
+
+// $.ajax({
+//     url: 'https://randomuser.me/api/',
+//     dataType: 'json',
+//     success: function(data){
+//         console.log(data.results);
+//     }
+// });
