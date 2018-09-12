36691d745599cfdb0613241310b998e8e1bd5ff3
Youssef Girgeis <youssefgirgeis@gmail.com>
2018-09-12 01:37:07 -0400

    tested son object

--- js/scripts.js
+++ js/scripts.js
@@ -6,20 +6,39 @@ $searchContainer.append('<form action="#" method="get"></form>');
 $('form').append('<input type="search" id="search-input" class="search-input" placeholder="Search...">');
 $('form').append('<input type="submit" value="&#x1F50D;" id="serach-submit" class="search-submit">');
 
-$gallary.append('<div class="card"></div>');
+
+
+                
+
+function createCards(){
+    $gallary.append('<div class="card"></div>');
 $('.card').append('<div class="card-img-container"></div>');
 $('.card-img-container').append('<img class="card-img" src="https://placehold.it/90x90" alt="profile picture">');
 
-$gallary.append('<div class="card-info-container"></div>');
+$('.card').append('<div class="card-info-container"></div>');
 $('.card-info-container').append('<h3 id="name" class="card-name cap">first last</h3>');
-$('.card-info-container').append('<p class="card-text">email</p>');
-$('.card-info-container').append('<p class="card-text cap">city, state</p>');
+$('.card-info-container').append('<p id="email" class="card-text">email</p>');
+$('.card-info-container').append('<p id="location" class="card-text cap">city, state</p>');
+
+}
+
+$.ajax({
+    url: 'https://randomuser.me/api?results=12',
+    dataType: 'json',
+    success: function(user){
+        console.log(user);
+        console.log(user.results);
+        console.log(user.results.length);
+        $.each(user.results, function(i, user){
+
+            $('#name').html(user.name.first + ' ' + user.name.last);
+            $('#email').html(user.email);
+            $('#location').html(user.location.city);
+            $('img').attr('src', user.picture.thumbnail);
+
+            createCards();
 
 
-// $.ajax({
-//     url: 'https://randomuser.me/api/',
-//     dataType: 'json',
-//     success: function(data){
-//         console.log(data.results);
-//     }
-// });
+        });
+    }
+});
