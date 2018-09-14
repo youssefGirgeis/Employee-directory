6711f34fbe0b2f2d6bd418b1badf50fa0f0f172b
Youssef Girgeis <youssefgirgeis@gmail.com>
2018-09-13 22:05:32 -0400

    posting results retrived from the API

--- js/scripts.js
+++ js/scripts.js
@@ -6,20 +6,27 @@ $searchContainer.append('<form action="#" method="get"></form>');
 $('form').append('<input type="search" id="search-input" class="search-input" placeholder="Search...">');
 $('form').append('<input type="submit" value="&#x1F50D;" id="serach-submit" class="search-submit">');
 
-$gallary.append('<div class="card"></div>');
-$('.card').append('<div class="card-img-container"></div>');
-$('.card-img-container').append('<img class="card-img" src="https://placehold.it/90x90" alt="profile picture">');
 
-$gallary.append('<div class="card-info-container"></div>');
-$('.card-info-container').append('<h3 id="name" class="card-name cap">first last</h3>');
-$('.card-info-container').append('<p class="card-text">email</p>');
-$('.card-info-container').append('<p class="card-text cap">city, state</p>');
+$.ajax({
+    url: 'https://randomuser.me/api/?results=12',
+    dataType: 'json',
+    success: function(data){
+        //console.log(data.results);
 
-
-// $.ajax({
-//     url: 'https://randomuser.me/api/',
-//     dataType: 'json',
-//     success: function(data){
-//         console.log(data.results);
-//     }
-// });
+        $.each(data.results, function(index, user){
+            console.log(user);
+            $gallary.append(`
+                <div class="card">
+                        <div class="card-img-container">
+                        <img class="card-img" src="${user.picture.thumbnail}" alt="profile picture">
+                    </div>
+                    <div class="card-info-container">
+                        <h3 id="name" class="card-name cap">${user.name.first} ${user.name.last}</h3>
+                        <p class="card-text">${user.email}</p>
+                        <p class="card-text cap">${user.location.city}</p>
+                    </div>
+                </div>
+            `);
+        });        
+    }
+});
